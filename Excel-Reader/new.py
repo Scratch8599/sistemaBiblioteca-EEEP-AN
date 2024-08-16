fcb488677e8045b9e464dfb2b2b735d4b1d50c97
Gabriel Silva <142753406+Scratch8599@users.noreply.github.com>
2024-08-16 15:52:34 +0000

    new.py updated with a new version

--- Excel-Reader/new.py
+++ Excel-Reader/new.py
@@ -1,4 +1,4 @@
-import os, time, pandas as pd
+import os, time, pandas as pd, psycopg2
 # Integração à planilha
 booksSpreadsheet = pd.read_excel(r"ACERVO BIBLIOGRÁFICO - CATALOGADO EM 2023.xlsx", header = 0)
 titlesCollumn    = booksSpreadsheet['TITULO DO LIVRO'].tolist()
@@ -11,16 +11,66 @@ quantitCollumn   = booksSpreadsheet['QUANT.'].tolist()
 
 # O "x".tolist() retorna uma lista float (???)
 
+conn = psycopg2.connect(database="    ", 
+                        user="    ", 
+                        password="    ")
+curr = conn.cursor()
+
+dbErrors = [ ]
+
+nameTable = str(input("Digite o nome da tabela a ser inserido"))
+
+
+
 cont = 0
+for i in titlesCollumn:
+    if str(authorsCollumn[cont]) != 'nan' or str(categoryCollumn[cont]) != 'nan' or str(editionCollumn[cont]) != 'nan' or str(yearCollumn[cont]) != 'nan' or str(publisherCollumn[cont]) != 'nan' or str(quantitCollumn[cont]) != 'nan':
+        curr.execute('''
+        INSERT INTO %s (Book, Author, Category, Edition, PublishYear, Publisher, Quantity) VALUES(%s, %s, %s, %s, %s, %s, %s)
+        ''', (nameTable, str(titlesCollumn[cont]), str(authorsCollumn[cont]), str(categoryCollumn[cont]), str(editionCollumn[cont]), int(yearCollumn[cont]), str(publisherCollumn[cont]), int(quantitCollumn[cont])))
+        curr.execute('''
+        SELECT * FROM %s WHERE Book = '%s' 
+        ''', (nameTable, str(titlesCollumn[cont])))
+        validation = curr.fetchall()
+        if str(titlesCollumn[cont]) == str(validation[1]):
+            print(f"Erro em {titlesCollumn[cont]}")
+            dbErrors.append(f"Erro em {titlesCollumn[cont]}")
+            pass
+        elif str(authorsCollumn[cont]) == str(validation[2]):
+            print(f"Erro em {authorsCollumn[cont]}")
+            dbErrors.append(f"Erro em {authorsCollumn[cont]}")
+            pass
+        elif str(categoryCollumn[cont]) == str(validation[3]):
+            print(f"Erro em {categoryCollumn[cont]}")
+            dbErrors.append(f"Erro em {categoryCollumn[cont]}")
+            pass
+        elif str(editionCollumn[cont]) == str(validation[4]):
+            print(f"Erro em {editionCollumn[cont]}")
+            dbErrors.append(f"Erro em {editionCollumn[cont]}")
+            pass
+        elif str(yearCollumn[cont]) == str(validation[5]):
+            print(f"Erro em {yearCollumn[cont]}")
+            dbErrors.append(f"Erro em {yearCollumn[cont]}")
+            pass
+        elif str(publisherCollumn[cont]) == str(validation[6]):
+            print(f"Erro em {publisherCollumn[cont]}")
+            dbErrors.append(f"Erro em {publisherCollumn[cont]}")
+            pass
+        elif str(quantitCollumn[cont]) == str(validation[7]):
+            print(f"Erro em {quantitCollumn[cont]}")
+            dbErrors.append(f"Erro em {quantitCollumn[cont]}")
+            pass
+        pass
+    else 
 
-# for i in range(0,20):
-#     print(f"Working{"."*cont}")
-#     cont += 1
-#     time.sleep(0.2)
-#     os.system('cls')
-#     if cont == 4:
-#         cont = 0
 
+
+
+
+
+
+# Old code, just backup
+cont = 0
 for i in titlesCollumn:
     # Condicional feita para dividir as seções: "PNLD LITERÁRIO 2021/2023 - ACERVO x"
     if str(quantitCollumn[cont]) == "nan":
